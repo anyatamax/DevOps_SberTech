fd8d2595dacb29c7e4704ee2a2764bf009ad07fa
Any <maksimova.am@phystech.edu>
2023-12-10 15:36:29 +0300

    add tests

--- my_app/test_main.py
+++ my_app/test_main.py
@@ -1,11 +1,14 @@
 import unittest
 from app import add, subtract
+import allure
 
 class TestMain(unittest.TestCase):
+    @allure.title("Test add")
     def test_add(self):
         result = add(5, 3)
         self.assertEqual(result, 8)
     
+    @allure.title("Test sub")
     def test_subtract(self):
         result = subtract(5, 3)
         self.assertEqual(result, 2)
